a0cea2d8fd6e8d67513c1b091e6ec43103dc4afd
Venkataramana Pai K <venkatpaik17@gmail.com>
2023-06-27 18:16:30 +0530

    comment sqlalchemy create engine definition since we are using alembic to do the same. Added CORS configuration using CORSMiddleware class

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
 from .routers import auth, post, user, vote
 
-# connection is established and tables are created
-models.Base.metadata.create_all(bind=engine)
+# connection is established and tables are created, only needed if using sqlalchemy for create operations. Not required if using alembic (DB Migr tool)
+# models.Base.metadata.create_all(bind=engine)
+
 
 # creating a fastapi instance
 app = FastAPI()
+
+# CORS (Cross Origin Resource Sharing)
+# define origins for CORS, this is useful if any other domain should gain access to our api endpoints. If CORS is not implemented by default browser implements SOP (Single Origin Policy)
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # order of path operations matters. Be careful while defining paths
